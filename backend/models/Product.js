7c551c3e7aa386a791b59ae025690f6413ccf6c5
zarrar79 <shahzarrar79@gmail.com>
2025-07-28 00:00:03 +0500

    APIs for the products areset

---
+++ backend/models/Product.js
@@ -0,0 +1,14 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  category: { type: String, required: true },
+  imageUrl: { type: String, required: true }
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
