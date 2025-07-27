cfe8a23e074c781727ad50096261d2dd8adc077f
zarrar79 <shahzarrar79@gmail.com>
2025-07-28 00:37:18 +0500

    Merge Feature

--- backend/server.js
+++ backend/server.js
@@ -178,4 +178,4 @@ app.delete("/products/:id", async (req, res) => {
 
 // ========================= Server Start ========================= //
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port   ${PORT}`));
