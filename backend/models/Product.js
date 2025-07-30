0b38f61d02d933aaa703c100eb5f2eafc1a4783f
zarrar79 <shahzarrar79@gmail.com>
2025-07-30 23:21:13 +0500

    Image Upload Completed

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -5,6 +5,7 @@ const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  brand : {type: String, required : true},
   quantity: { type: Number, required: true },
   category: { type: String, required: true },
   imageUrl: { type: String, required: true }
